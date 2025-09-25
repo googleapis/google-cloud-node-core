175664b2cfd0f064dad732cca767d5812d1568e6
Gautam Sharda <gautamsharda@google.com>
2025-09-25 00:47:45 +0000

    change copyright to 2025

--- dev-packages/jsdoc-fresh/.jsdoc.js
+++ dev-packages/jsdoc-fresh/.jsdoc.js
@@ -1,5 +1,5 @@
 /*!
- * Copyright 2018 Google LLC. All Rights Reserved.
+ * Copyright 2025 Google LLC. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -37,7 +37,7 @@ module.exports = {
     includePattern: '\\.js$'
   },
   templates: {
-    copyright: 'Copyright 2018 Google, LLC.',
+    copyright: 'Copyright 2025 Google, LLC.',
     includeDate: false,
     sourceFiles: false,
     systemName: 'jsdoc-fresh',
