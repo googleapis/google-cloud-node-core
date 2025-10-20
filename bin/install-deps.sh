361f351c9c2f82966b7c2b1641ae1adfa472bd6d
miguel <miguelvelezsa@google.com>
2025-10-20 22:03:36 +0000

    feat: add preinstall script

---
+++ bin/install-deps.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# A script to install dependencies for specified packages in order.
+
+# Exit on any error.
+set -eo pipefail
+
+echo "Installing dependencies in order..."
+
+# Define the packages to install in the desired order.
+# These will be installed from the npm registry.
+declare -a packages=(
+    "jsdoc-fresh"
+    "pack-n-play"
+    "jsdoc-region-tag"
+    "gaxios"
+    "gcp-metadata"
+)
+
+for package in "${packages[@]}"; do
+    echo "--- Installing $package ---"
+    npm install "$package"
+done
+
+echo "All specified dependencies installed successfully."
