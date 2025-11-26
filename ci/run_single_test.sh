962827e0bdec26cd07e9e395017496d659f54aa1
Sofia Leon <sofialeon@google.com>
2025-11-26 13:29:31 -0800

    chore: fix

--- ci/run_single_test.sh
+++ ci/run_single_test.sh
@@ -41,6 +41,11 @@ fi
 
 retval=0
 
+# In this run, we're running both 1) cascading tests, as well as
+# 2) individual tests for a given package. The reason we run them separately
+# 3) is because cascading tests need to run without compilation; so, we're
+# 4) also running individual package tests to ensure compilation is done correctly
+
 set +e
 case ${TEST_TYPE} in
 lint)
@@ -51,14 +56,20 @@ lint)
     ;;
 samples)
     ${PROJECT_ROOT}/bin/run-interdependent-tests.sh "samples-test"
+    npm install --ignore-scripts --engine-strict; npm install
+    npm run samples-test
     retval=$?
     ;;
 system)
     ${PROJECT_ROOT}/bin/run-interdependent-tests.sh "system-test"
+    npm install --ignore-scripts --engine-strict; npm install
+    npm run system-test
     retval=$?
     ;;
 units)
     ${PROJECT_ROOT}/bin/run-interdependent-tests.sh "test"
+    npm install --ignore-scripts --engine-strict; npm install
+    npm run test
     retval=$?
     ;;
 *)
